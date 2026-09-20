0b80d8cc03c473fc46796d728ade996b19ec11d3
Andry7Rz <andryhernandezpacheco@gmail.com>
2026-09-20 14:12:29 -0400

    waza

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>hola pe causas</h1>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
